b6ce2b27a4b41aabb5737437d035056c84bac587
Jeston Lewis <61254261+jestoncolelewis@users.noreply.github.com>
2025-02-02 22:11:31 -0700

    added basic structure

---
+++ substrings.rb
@@ -0,0 +1,6 @@
+def substrings(text, dictionary)
+  puts text, dictionary
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("Howdy partner, sit down! How's it going?", dictionary)
