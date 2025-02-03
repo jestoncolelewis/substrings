7eadd8c787cb9182a8c3f9bf997c402738595616
Jeston Lewis <61254261+jestoncolelewis@users.noreply.github.com>
2025-02-02 22:30:03 -0700

    added initial logic

--- substrings.rb
+++ substrings.rb
@@ -1,6 +1,13 @@
+require "pry-byebug"
 def substrings(text, dictionary)
-  puts text, dictionary
+  counted_subs = Hash.new
+  dictionary.each do |substring|
+    if text.downcase.include?(substring)
+      counted_subs[substring] = text.count(substring)
+    end
+  end
+  counted_subs
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("Howdy partner, sit down! How's it going?", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
